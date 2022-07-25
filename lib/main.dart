786ae5c3bce48f65082326f441c1235999645722
Talha <yucelltalha@gmail.com>
2022-07-25 10:10:36 +0300

    new version relased

--- lib/main.dart
+++ lib/main.dart
@@ -1,235 +1,167 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:sensors_plus/sensors_plus.dart';
-import 'package:vibration_catcher/streamControl.dart';
 
-import 'acce2.dart';
+void main() {
+  runApp(const HomePage());
+}
 
-void main() => runApp(MyApp3());
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
 
-class MyApp extends StatelessWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var axisStyle = const TextStyle(
+      fontFamily: "Ibarra", fontWeight: FontWeight.bold, fontSize: 18);
+  bool recording = false;
+  StreamSubscription? streamSub;
+  UserAccelerometerEvent? event;
+  String recordStat = "Start Recording";
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Accelerometer',
-
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        // Define the default brightness and colors.
+        // brightness: Brightness.dark,
+        primaryColor: Colors.green,
       ),
-      home: MyHomePage(title: 'Accelerometer'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Vibration Analyzer"),
+          centerTitle: true,
+          backgroundColor: Colors.green,
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  "Axis Values",
+                  style: TextStyle(
+                    fontStyle: FontStyle.normal,
+                    fontSize: 22.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 12, left: 3, right: 3),
+                padding: const EdgeInsets.only(bottom: 12.0, top: 8),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.green.shade200, width: 3),
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Column(
+                      children: [
+                        Text(
+                          "X",
+                          style: axisStyle,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 8.0),
+                          child:
+                              Text(event?.x.toStringAsFixed(3) ?? "Değer Yok"),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Text(
+                          "Y",
+                          style: axisStyle,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 8.0),
+                          child:
+                              Text(event?.y.toStringAsFixed(3) ?? "Değer Yok"),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Text(
+                          "Z",
+                          style: axisStyle,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 8.0),
+                          child:
+                              Text(event?.z.toStringAsFixed(3) ?? "Değer Yok"),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              const Divider(height: 200),
+              ElevatedButton(
+                style: colorFul(Colors.red, Colors.green),
+                onPressed: () {
+                  setState(
+                    () {
+                      if (!recording) {
+                        start();
+                      } else {
+                        stop();
+                      }
+                    },
+                  );
+                },
+                child: Text(recordStat),
+              )
+            ],
+          ),
+        ),
+      ),
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({required this.title});
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  // color of the circle
-  Color color = Colors.greenAccent;
-
-  // event returned from accelerometer stream
-  AccelerometerEvent? event;
-
-  // hold a refernce to these, so that they can be disposed
-  Timer? timer;
-  StreamSubscription? accel;
-
-  // positions and count
-  double top = 125;
-  double? left;
-  int count = 0;
-
-  // variables for screen size
-  double? width;
-  double? height;
-
-  setColor(AccelerometerEvent event) {
-    // Calculate Left
-    double x = ((event.x * 12) + ((width! - 100) / 2));
-    // Calculate Top
-    double y = event.y * 12 + 125;
-
-    // find the difference from the target position
-    var xDiff = x.abs() - ((width! - 100) / 2);
-    var yDiff = y.abs() - 125;
-
-    // check if the circle is centered, currently allowing a buffer of 3 to make centering easier
-    if (xDiff.abs() < 3 && yDiff.abs() < 3) {
-      // set the color and increment count
-      setState(() {
-        color = Colors.greenAccent;
-        count += 1;
-      });
-    } else {
-      // set the color and restart count
-      setState(() {
-        color = Colors.red;
-        count = 0;
-      });
-    }
-  }
-
-  setPosition(AccelerometerEvent event) {
-    if (event == null) {
-      return;
-    }
-
-    // When x = 0 it should be centered horizontally
-    // The left positin should equal (width - 100) / 2
-    // The greatest absolute value of x is 10, multipling it by 12 allows the left position to move a total of 120 in either direction.
-    setState(() {
-      left = ((event.x * 12) + ((width! - 100) / 2));
-    });
-
-    // When y = 0 it should have a top position matching the target, which we set at 125
-    setState(() {
-      top = event.y * 12 + 125;
-    });
-  }
-
-
-  startTimer() {
-    // if the accelerometer subscription hasn't been created, go ahead and create it
-    if (accel == null) {
-      accel = accelerometerEvents.listen((AccelerometerEvent eve) {
-        setState(() {
-          event = eve;
-        });
-      });
-    } else {
-      // it has already ben created so just resume it
-      accel?.resume();
-    }
-
-    // Accelerometer events come faster than we need them so a timer is used to only proccess them every 200 milliseconds
-    if (timer == null || !timer!.isActive) {
-      timer = Timer.periodic(Duration(milliseconds: 200), (_) {
-        // if count has increased greater than 3 call pause timer to handle success
-        if (count > 3) {
-          pauseTimer();
-        } else {
-          // proccess the current event
-          setColor(event!);
-          setPosition(event!);
-        }
-      });
-    }
-  }
-
-  pauseTimer() {
-    // stop the timer and pause the accelerometer stream
-    timer?.cancel();
-    accel?.pause();
-
-    // set the success color and reset the count
-    setState(() {
-      count = 0;
-      color = Colors.green;
-    });
   }
 
   @override
   void dispose() {
-    timer?.cancel();
-    accel?.cancel();
+    streamSub?.cancel();
     super.dispose();
   }
 
-  @override
-  Widget build(BuildContext context) {
-    // get the width and height of the screen
-    width = MediaQuery.of(context).size.width;
-    height = MediaQuery.of(context).size.height;
+  start() {
+    print("recording");
+    streamSub = userAccelerometerEvents.listen((UserAccelerometerEvent eve) {
+      setState(() {
+        event = eve;
+      });
+    });
+    recording = !recording;
 
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Column(
-        children: [
-          const Padding(
-            padding: EdgeInsets.all(8.0),
-            child: Text('Keep the circle in the center for 1 second'),
-          ),
-          Stack(
-            children: [
-              // This empty container is given a width and height to set the size of the stack
-              SizedBox(
-                height: height! / 2,
-                width: width,
-              ),
+    setState(() {
+      recordStat = "Stop Recording";
+    });
+  }
 
-              // Create the outer target circle wrapped in a Position
-              Positioned(
-                // positioned 50 from the top of the stack
-                // and centered horizontally, left = (ScreenWidth - Container width) / 2
-                top: 50,
-                left: (width! - 250) / 2,
-                child: Container(
-                  height: 250,
-                  width: 250,
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.red, width: 2.0),
-                    borderRadius: BorderRadius.circular(125),
-                  ),
-                ),
-              ),
-              // This is the colored circle that will be moved by the accelerometer
-              // the top and left are variables that will be set
-              Positioned(
-                top: top,
-                left: left ?? (width! - 100) / 2,
-                // the container has a color and is wrappeed in a ClipOval to make it round
-                child: ClipOval(
-                  child: Container(
-                    width: 100,
-                    height: 100,
-                    color: color,
-                  ),
-                ),
-              ),
-              // inner target circle wrapped in a Position
-              Positioned(
-                top: 125,
-                left: (width! - 100) / 2,
-                child: Container(
-                  height: 100,
-                  width: 100,
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.green, width: 2.0),
-                    borderRadius: BorderRadius.circular(50),
-                  ),
-                ),
-              ),
-            ],
-          ),
-          Text('x: ${(event?.x ?? 0).toStringAsFixed(3)}'),
-          Text('y: ${(event?.y ?? 0).toStringAsFixed(3)}'),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-            child: RaisedButton(
-              onPressed: startTimer,
-              color: Theme.of(context).primaryColor,
-              textColor: Colors.white,
-              child: const Text('Begin'),
-            ),
-          ),
-          Container(
-            alignment: Alignment.center,
-            width: 200,
-            height: 40,
-            color: Colors.grey.shade200,
-            child: Text("Deneme"),
-          )
-        ],
-      ),
-    );
+  stop() {
+    print("Record Stopped");
+
+    setState(() {
+      recordStat = "Start Recording";
+    });
+    streamSub?.cancel();
+    streamSub = null;
+    event = null;
+    recording = !recording;
+  }
+
+  colorFul(Color color1, Color color2) {
+    var buttonStyle = ButtonStyle(
+        backgroundColor: recording
+            ? MaterialStateProperty.all(color1)
+            : MaterialStateProperty.all(color2));
+    return buttonStyle;
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:vibration_catcher/main.dart';
+import 'package:vibration_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(const HomePage());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
